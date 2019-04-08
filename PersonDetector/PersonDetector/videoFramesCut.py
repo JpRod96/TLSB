a87e4466568aee8a5609ad641dd53ed4bbf1eb7d
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-04-08 18:02:20 -0400

    Video Frames Cut

---
+++ PersonDetector/PersonDetector/videoFramesCut.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('omg.mp4')
+#numero total de frames
+frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+cutOn = 0
+cont = 0
+listOfFrames = []
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    cont += 1
+    if cont >= cutOn:
+        listOfFrames.append(frame)
+        # Corte en el frame:
+        cutOn = frameCount/5 + cont
+        #print(cont)
+    else:
+        if cutOn >= frameCount:
+            break
+
+for photo in listOfFrames:
+    cv2.imshow('photo',photo)
+    cv2.waitKey(0)
+
+cap.release()
+cv2.destroyAllWindows()
