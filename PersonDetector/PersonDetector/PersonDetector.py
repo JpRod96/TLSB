a97e6ce322f2ee0f350e5f9826c3bf20dfa8265f
JpRod <jprodriguez60@gmail.com>
2019-04-09 12:07:08 -0400

    Created PersonDetector class and its new method detectPerson, which is based in the previous behaviour of this file

--- PersonDetector/PersonDetector/PersonDetector.py
+++ PersonDetector/PersonDetector/PersonDetector.py
@@ -1,18 +1,22 @@
 from imageai.Detection import ObjectDetection
 import os
+class PersonDetector:
 
-execution_path = os.getcwd()
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
-detector.loadModel()
-custom_objects = detector.CustomObjects(person=True)
-detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), minimum_percentage_probability=55)
+    def detectPerson(self, imageName):
+        execution_path = os.getcwd()
+        detector = ObjectDetection()
+        detector.setModelTypeAsRetinaNet()
+        detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+        detector.loadModel()
 
-for detection in detections:
-    print(detection["name"] , " : " , detection["percentage_probability"] )
+        custom_objects = detector.CustomObjects(person=True)
+        detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=os.path.join(execution_path , imageName), output_image_path=os.path.join(execution_path , imageName + "new.jpg"), minimum_percentage_probability=55)
+
+        for detection in detections:
+            print(detection["name"] , " : " , detection["percentage_probability"], ":", detection["box_points"])
 
 
-#detections, extracted_images = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), 
-#                                                               output_image_path=os.path.join(execution_path, "imagenew.jpg"), 
-#                                                               extract_detected_objects=True)
+        #detections, extracted_images = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), 
+        #                                                               output_image_path=os.path.join(execution_path, "imagenew.jpg"), 
+        #                                                               extract_detected_objects=True)
+        
