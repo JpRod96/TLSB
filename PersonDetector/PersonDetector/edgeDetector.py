20ae21fae739bdf71b7bd40f9e27806e9ff8f7c3
JpRod <jprodriguez60@gmail.com>
2019-04-10 16:31:31 -0400

    First approach to squaring a picture

--- PersonDetector/PersonDetector/edgeDetector.py
+++ PersonDetector/PersonDetector/edgeDetector.py
@@ -24,6 +24,23 @@ class EdgeDetector:
         cv2. imwrite (imageName + "edges" + extension, canny)
 
 
+    def squaringPicture(self, img):
+
+        #get size
+        height, width, channels = img.shape
+        print (in_img,height, width, channels)
+        # Create a black image
+        x = height if height > width else width
+        y = height if height > width else width
+        square= np.zeros((x,y,3), np.uint8)
+        #
+        #This does the job
+        #
+        square[(y-height)/2:y-(y-height)/2, (x-width)/2:x-(x-width)/2] = img
+        cv2.imwrite(out_img,square)
+        cv2.imshow("original", img)
+        cv2.imshow("black square", square)
+        cv2.waitKey(0)
 
         ## Buscamos los contornos
         #(contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
