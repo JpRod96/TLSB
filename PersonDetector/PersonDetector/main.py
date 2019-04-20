8116b45cb96a43dbca2cd3b6fc6a896d1aa012f1
JpRod <jprodriguez60@gmail.com>
2019-04-20 10:10:16 -0400

    Extracted new videoCutter class form main code

--- PersonDetector/PersonDetector/main.py
+++ PersonDetector/PersonDetector/main.py
@@ -2,40 +2,19 @@ import numpy as np
 import cv2
 from PersonDetector import PersonDetector
 from edgeDetector import EdgeDetector
+from videoCutter import VideoCutter
 
-PHOTO="photo"
-JPG_EXTENSION=".jpg"
-cap = cv2.VideoCapture('sample.mp4')
-print("Video loaded")
-#numero total de frames
-frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+FRAMES_NRO=5
+PHOTO = "photo"
+JPG_EXTENSION = ".jpg"
 
-cutOn = 0
-cont = 0
-listOfFrames = []
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    cont += 1
-    if cont >= cutOn:
-        listOfFrames.append(frame)
-        # Corte en el frame:
-        cutOn = frameCount/5 + cont
-    else:
-        if cutOn >= frameCount:
-            break
-
-numberOfFrame = 1
-for photo in listOfFrames:
-    imageName = PHOTO + str(numberOfFrame) + JPG_EXTENSION;
-    cv2.imwrite (imageName, photo)
-    numberOfFrame += 1
-
-cap.release()
-detector = PersonDetector();
+videoCutter =  VideoCutter()
+frames = videoCutter.cutVideo("sample.mp4", FRAMES_NRO)
+videoCutter.saveFrames(frames)
+detector = PersonDetector()
 edgeDetector = EdgeDetector()
-listOfTreatedImages = []
-for x in range(1, 6):
+
+for x in range(1, FRAMES_NRO+1):
     imageName = PHOTO + str(x)
     print(imageName + JPG_EXTENSION)
     treatedImageName = detector.detectPerson(imageName, JPG_EXTENSION)
---
+++ PersonDetector/PersonDetector/videoCutter.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+class VideoCutter:
+    PHOTO="photo"
+    JPG_EXTENSION=".jpg"
+
+    def cutVideo(self, videoPath, outFramesNumber):
+        cap = cv2.VideoCapture(videoPath)
+        print("Video loaded")
+        #numero total de frames
+        frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        cutOn = 0
+        cont = 0
+        listOfFrames = []
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            cont += 1
+            if cont >= cutOn:
+                listOfFrames.append(frame)
+                # Corte en el frame:
+                cutOn = frameCount/outFramesNumber + cont
+            else:
+                if cutOn >= frameCount:
+                    break
+        cap.release()
+        return listOfFrames
+
+    def saveFrames(self, listOfFrames):
+        numberOfFrame = 1
+        for photo in listOfFrames:
+            imageName = self.PHOTO + str(numberOfFrame) + self.JPG_EXTENSION;
+            cv2.imwrite (imageName, photo)
+            numberOfFrame += 1
