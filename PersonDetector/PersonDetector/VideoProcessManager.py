47c576f525892111dec09e7eedbfd15d1e5d49af
JpRod <jprodriguez60@gmail.com>
2019-04-23 20:39:37 -0400

    Processing videos of directory

--- PersonDetector/PersonDetector/VideoProcessManager.py
+++ PersonDetector/PersonDetector/VideoProcessManager.py
@@ -1,14 +1,35 @@
 from os import listdir
 from os.path import isfile, join
+from videoProcessor import VideoProcessor
 import util
 
 class VideoProcessManager:
 
-    def isGivenPathAFile(self, path):
-        finalToken = util.getLastTokenOfPath(path)
-        return len(finalToken)>1
+    framesNro = None
+    videoProcessor = None
+    MP4_EXTENSION = "mp4"
+
+    def __init__(self, framesNro):
+        self.framesNro = framesNro
+
+    def isGivenFileMP4File(self, file):
+        finalToken = util.getLastTokenOfPath(file)
+        return finalToken[1] == self.MP4_EXTENSION
     
     def isGivenPathADir(self, path):
         finalToken = util.getLastTokenOfPath(path)
-        return len(finalToken)==1
-        
+        return len(finalToken) == 1
+    
+    def processPath(self, path):
+        self.videoProcessor = VideoProcessor()
+        if self.isGivenPathADir(path) :
+            videoFiles = self.getVideoFilesFromDirectory(path)
+            for videoFile in videoFiles:
+                self.videoProcessor.process(path + "/" + videoFile, self.framesNro)
+        else :
+            self.videoProcessor.process(path, self.framesNro)
+
+    def getVideoFilesFromDirectory(self, path):
+        files = [f for f in listdir(path) if isfile(join(path, f))]
+        videoFiles = list(filter(lambda x: self.isGivenFileMP4File(x) , files))
+        return videoFiles
--- PersonDetector/PersonDetector/main.py
+++ PersonDetector/PersonDetector/main.py
@@ -1,7 +1,13 @@
 from videoProcessor import VideoProcessor
 import util
+from VideoProcessManager import VideoProcessManager
 
 FRAMES_NRO=5
 
-videoProcessor = VideoProcessor()
-videoProcessor.process("sample.mp4", FRAMES_NRO);
+#videoProcessor = VideoProcessor()
+#videoProcessor.process("sample.mp4", FRAMES_NRO);
+
+vid = VideoProcessManager(5)
+
+vid.processPath("C:/Users/Jp.SANDRO-HP/Desktop/wipi.mp4")
+vid.processPath("C:/Users/Jp.SANDRO-HP/Desktop")
