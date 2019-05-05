3c147e221d0aa4babc82c02d1dda1afa90f3b92c
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-05-05 15:42:11 -0400

    PROCESAMIENTO DE IMAGENES BALCO/NEGRO

--- PersonDetector/PersonDetector/edgeDetector.py
+++ PersonDetector/PersonDetector/edgeDetector.py
@@ -25,12 +25,13 @@ class EdgeDetector:
         #resizedImg = cv2.resize(squarePic, (self.pictureSize, self.pictureSize))
 
         #PRUEBA
-        med_val = np.median(image)
-        lower = int(max(0,0.7*med_val))
-        upper = int(min(255,1.3*med_val))
-        blurred_img = cv2.blur(image,(5,5))
-        canny = cv2.Canny(blurred_img, lower, upper)
-        squarePic = self.make_square(canny)
+        #med_val = np.median(image)
+        #lower = int(max(0,0.7*med_val))
+        #upper = int(min(255,1.3*med_val))
+        #blurred_img = cv2.blur(image,(5,5))
+        #canny = cv2.Canny(blurred_img, lower, upper)
+        gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        squarePic = self.make_square(gris)
         resizedImg = cv2.resize(squarePic, (self.pictureSize, self.pictureSize))
         return resizedImg
 
--- PersonDetector/PersonDetector/main.py
+++ PersonDetector/PersonDetector/main.py
@@ -9,28 +9,28 @@ typeOfCut = "Constant"
 
 vid = VideoProcessManager(framesNro, picSize, rotate, typeOfCut)
 #vid.processPath("C:/Users/Jp.SANDRO-HP/Downloads/gestos/luz")
-#vid.processPath("C:/Users/Nicolas/Desktop/VideosDataset/BAÑO1")
+#vid.processPath("D:/VideosDataset/BANO1")
 
-#i = 1
-#print("Generating edges of images for word Bano")
-#while i < 6:
-#  vid.processPath("D:/VideosDataset/BAÑO" + str(i))
-#  i += 1
-#print("Word Bathroom complete")
+i = 1
+print("Generating edges of images for word Bano")
+while i < 6:
+  vid.processPath("D:/VideosDataset/BANO" + str(i))
+  i += 1
+print("Word Bathroom complete")
 
-#i = 1
-#print("Generating edges of images for word BuenosDias")
-#while i < 6:
-#  vid.processPath("D:/VideosDataset/BUENOSDIAS" + str(i))
-#  i += 1
-#print("Word BuenosDias complete")
+i = 1
+print("Generating edges of images for word BuenosDias")
+while i < 6:
+  vid.processPath("D:/VideosDataset/BUENOSDIAS" + str(i))
+  i += 1
+print("Word BuenosDias complete")
 
-#i = 1
-#print("Generating edges of images for word Hola")
-#while i < 6:
-#  vid.processPath("D:/VideosDataset/HOLA" + str(i))
-#  i += 1
-#print("Word Hola complete")
+i = 1
+print("Generating edges of images for word Hola")
+while i < 6:
+  vid.processPath("D:/VideosDataset/HOLA" + str(i))
+  i += 1
+print("Word Hola complete")
 
 i = 1
 print("Generating edges of images for word Luz")
