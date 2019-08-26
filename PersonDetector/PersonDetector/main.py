44ed7fc4449b78c4eb5a7d7bd08c8de4ac880532
JpRod96 <jprodriguez60@gmail.com>
2019-08-26 09:26:19 -0400

    Adding some documentation of usage of the core

--- PersonDetector/PersonDetector/main.py
+++ PersonDetector/PersonDetector/main.py
@@ -9,13 +9,26 @@ rotate = True
 # typeOfCut = "Constant" || "Probabilistic"
 typeOfCut = "Constant"
 combineImages = False
-filter = 'Blury_Edges 1 1' #'None' 'Edges' 'Grayscale' Blury Edges'
+filter = 'Blury_Edges 1 1' # usando kernel de 1x1
+"""
+  tipos de filtros
+  'None' - ningun filtro, devuelve las imagenes detetadas en rgb y el tamaño de imagen pedido
+  'Edges kH kW' - usa detection de bordes por blur guasiano, kH y kW son los tamaños que tendra el kernel del blur 
+                  ejemplo: 'Edges 2 4', usara un kernel 2x4, sino se proporcionan los tamaños, se usara un kernel de 5x5
+                  ejemplo: 'Edges', usara un kernel de 5x5 
+                  Al final se devuelve un imagen en escala de grises con los bordes detectados
+  'Grayscale' - usa el filtro de escala de grises y devuelve una imagen con el tamaño pedido
+  'Blury_Edges' - usa detection de bordes por blur de Opencv, kH y kW son los tamaños que tendra el kernel del blur 
+                  ejemplo: 'Blury_Edges 2 4', usara un kernel 2x4, sino se proporcionan los tamaños, se usara un kernel de 5x5
+                  ejemplo: 'Blury_Edges', usara un kernel de 5x5 
+                  Al final se devuelve un imagen en escala de grises con los bordes detectados
+"""
 
 #videoProcessor = VideoCutterProcessor(framesNro, picSize, rotate, typeOfCut)
 videoProcessor = VideoMotionProcessor(picSize, combineImages, filter=filter)
 
-#vid = VideoProcessManager(videoProcessor)
-#vid.processPath("D:/desktop/DATASET/CBBA")
+vid = VideoProcessManager(videoProcessor)
+vid.processPath("D:/desktop/DATASET/CBBA")
 
 videoProcessor.rotateImages = True
 videoProcessor.imageFilter = 'Edges'
