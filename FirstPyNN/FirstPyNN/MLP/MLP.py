ef5b2e2d7b4e12dd50789c29d8b3b0276bee0fb4
JpRod96 <jprodriguez60@gmail.com>
2019-09-15 08:30:19 -0400

    General refactor to mlp class and added support for saving the best model of a test session

--- FirstPyNN/FirstPyNN/MLP/MLP.py
+++ FirstPyNN/FirstPyNN/MLP/MLP.py
@@ -3,264 +3,227 @@ from dataSetCharger import DataSetCharger
 import numpy as np
 import tensorflow as tf
 
-fullPathTrain = "D:/desktop/TLSB/FirstPyNN/FirstPyNN/DATASET/train/"
-fullPathTest = "D:/desktop/TLSB/FirstPyNN/FirstPyNN/DATASET/test/"
-charger = DataSetCharger()
 
-"""
-HOLA_VALUE = 0
-AUTO_VALUE = 1
-CAFE_VALUE = 2
-ADIOS_VALUE = 3
-GRACIAS_VALUE = 4
-CBBA_VALUE = 5
-CUAL_VALUE = 6
-POR_FAVOR_VALUE = 7
-QUERER_VALUE = 8
-YO_VALUE = 9
-"""
+class MLPTester:
 
-HOLA_VALUE = 0
-AUTO_VALUE = 1
-CAFE_VALUE = 2
-ADIOS_VALUE = 3
-GRACIAS_VALUE = 0
-CBBA_VALUE = 1
-CUAL_VALUE = 2
-POR_FAVOR_VALUE = 3
-QUERER_VALUE = 4
-YO_VALUE = 9
+    def __init__(self, use_image_set, folders, switcher, path_train, path_test, flatten=False):
+        self.flag = use_image_set
+        self.folders = folders
+        self.switcher = switcher
+        self.path_train = path_train
+        self.path_test = path_test
+        self.flatten = flatten
+        self.charger = DataSetCharger()
+        self.best_score = 0
 
-HOLA = "HOLA"
-AUTO = "AUTO"
-CAFE = "CAFE"
-ADIOS = "ADIOS"
-GRACIAS = "GRACIAS"
-CBBA = "CBBA"
-CUAL = "CUAL"
-POR_FAVOR = "POR_FAVOR"
-QUERER = "QUERER"
-YO = "YO"
+    def start(self):
+        train, train_labels, test, test_labels, models = self.setup()
+        self.test_models(models, train, train_labels, test, test_labels)
 
-
-def main(flag, flatten=False):
-    train, train_labels, test, test_labels, models = setup(flag, flatten)
-    test_models(models, train, train_labels, test, test_labels)
-
-
-def setup(flag, flatten):
-    if flag:
-        train, train_labels, test, test_labels = load_set(flag, flatten)
-        models = image_models()
-    else:
-        train, train_labels, test, test_labels = load_set(flag, flatten)
-        models = lbp_models()
-    return train, train_labels, test, test_labels, models
-
-
-def test_models(models, train, train_labels, test, test_labels):
-    over_all_history = []
-    for index in range(0, len(models)):
-        model, epochs = models[index]
-        weights = model.get_weights()
-        over_all_history.append(
-            "--------------------------------------------------------------------------------------------------------")
-        over_all_history.append("test No: " + str(index))
-        results = []
-        for y in range(0, 15):
-            model_accuracy, history = train_model(model, train, train_labels, test, test_labels, epochs, weights)
-            results.append((model_accuracy, history.history))
-        over_all_history.append(results)
-
-    save_test_to_txt(over_all_history)
-
-
-def load_set(flag, flatten):
-    folders = [GRACIAS, CBBA, CUAL, POR_FAVOR, QUERER]
-    switcher = {
-        HOLA: HOLA_VALUE,
-        AUTO: AUTO_VALUE,
-        CAFE: CAFE_VALUE,
-        ADIOS: ADIOS_VALUE,
-        GRACIAS: GRACIAS_VALUE,
-        CBBA: CBBA_VALUE,
-        CUAL: CUAL_VALUE,
-        POR_FAVOR: POR_FAVOR_VALUE,
-        QUERER: QUERER_VALUE,
-        YO: YO_VALUE
-    }
-    if flag:
-        train, train_labels = charger.get_custom_image_data_set(fullPathTrain, folders, switcher, flatten)
-        test, test_labels = charger.get_custom_image_data_set(fullPathTest, folders, switcher, flatten)
-    else:
-        train, train_labels = charger.get_custom_lbp_data_set(fullPathTrain, folders, switcher)
-        test, test_labels = charger.get_custom_lbp_data_set(fullPathTest, folders, switcher)
-
-    print(train.shape)
-    print(train_labels)
-    print(test.shape)
-    print(test_labels)
-
-    return train, train_labels, test, test_labels
-
-
-def save_test_to_txt(history_array):
-    f = open("testResults.txt", "w+")
-    f.write(format_array(history_array))
-    f.close()
-
-
-def format_array(history_array):
-    string = ""
-    for token in history_array:
-        if isinstance(token, list):
-            for history in token:
-                string += format_token(history)
+    def setup(self):
+        if self.flag:
+            models = self.image_models()
         else:
-            string += token + "\n"
-    return string
+            models = self.lbp_models()
+        train, train_labels, test, test_labels = self.load_set()
 
+        return train, train_labels, test, test_labels, models
 
-def format_token(token):
-    string = ""
-    accuracy, history = token
-    string += "Test accuracy: " + str(accuracy) + "\n"
-    string += str(history) + "\n\n"
-    return string
+    def test_models(self, models, train, train_labels, test, test_labels):
+        over_all_history = []
+        for index in range(0, len(models)):
+            model, epochs = models[index]
+            weights = model.get_weights()
+            over_all_history.append(
+                "--------------------------------------------------------------------------------------------------------")
+            over_all_history.append("test No: " + str(index))
+            results = []
+            for y in range(0, 15):
+                model_accuracy, history = self.train_model(model, train, train_labels, test, test_labels, epochs,
+                                                           weights)
+                results.append((model_accuracy, history.history))
+            over_all_history.append(results)
 
+        self.save_test_to_txt(over_all_history)
 
-def image_models():
-    trainable_models = []
+    def load_set(self):
+        if self.flag:
+            train, train_labels = self.charger.get_custom_image_data_set(self.path_train, self.folders, self.switcher,
+                                                                         self.flatten)
+            test, test_labels = self.charger.get_custom_image_data_set(self.path_test, self.folders, self.switcher,
+                                                                       self.flatten)
+        else:
+            train, train_labels = self.charger.get_custom_lbp_data_set(self.path_train, self.folders, self.switcher)
+            test, test_labels = self.charger.get_custom_lbp_data_set(self.path_test, self.folders, self.switcher)
 
-    model0 = keras.Sequential([
-        keras.layers.Flatten(input_shape=(1500, 300)),
-        keras.layers.Dense(32, activation=tf.nn.sigmoid),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+        print(train.shape)
+        print(train_labels)
+        print(test.shape)
+        print(test_labels)
 
-    model0.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+        return train, train_labels, test, test_labels
 
-    model1 = keras.Sequential([
-        keras.layers.Flatten(input_shape=(1500, 300)),
-        keras.layers.Dense(16, activation=tf.nn.sigmoid),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+    def save_test_to_txt(self, history_array):
+        f = open("testResults.txt", "w+")
+        f.write(self.format_array(history_array))
+        f.close()
 
-    model1.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+    def format_array(self, history_array):
+        string = ""
+        for token in history_array:
+            if isinstance(token, list):
+                for history in token:
+                    string += self.format_token(history)
+            else:
+                string += token + "\n"
+        return string
 
-    model2 = keras.Sequential([
-        keras.layers.Flatten(input_shape=(1500, 300)),
-        keras.layers.Dense(16, activation=tf.nn.sigmoid),
-        keras.layers.Dense(16, activation=tf.nn.relu),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+    @staticmethod
+    def format_token(token):
+        string = ""
+        accuracy, history = token
+        string += "Test accuracy: " + str(accuracy) + "\n"
+        string += str(history) + "\n\n"
+        return string
 
-    model2.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
-    # parece el mejor
-    model3 = keras.Sequential([
-        keras.layers.Flatten(input_shape=(1500, 300)),
-        keras.layers.Dense(8, activation=tf.nn.sigmoid),
-        keras.layers.Dense(8, activation=tf.nn.sigmoid),
-        keras.layers.Dense(8, activation=tf.nn.relu),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+    @staticmethod
+    def image_models():
+        trainable_models = []
 
-    model3.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+        model0 = keras.Sequential([
+            keras.layers.Flatten(input_shape=(1500, 300)),
+            keras.layers.Dense(32, activation=tf.nn.sigmoid),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-    trainable_models.append((model0, 15))
-    trainable_models.append((model0, 40))
-    trainable_models.append((model1, 30))
-    trainable_models.append((model1, 60))
-    trainable_models.append((model2, 60))
-    trainable_models.append((model2, 80))
-    trainable_models.append((model3, 80))
-    trainable_models.append((model3, 160))
+        model0.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
-    return trainable_models
+        model1 = keras.Sequential([
+            keras.layers.Flatten(input_shape=(1500, 300)),
+            keras.layers.Dense(16, activation=tf.nn.sigmoid),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
+        model1.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
-def lbp_models():
-    trainable_models = []
+        model2 = keras.Sequential([
+            keras.layers.Flatten(input_shape=(1500, 300)),
+            keras.layers.Dense(16, activation=tf.nn.sigmoid),
+            keras.layers.Dense(16, activation=tf.nn.relu),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-    model0 = keras.Sequential([
-        keras.layers.Dense(26, input_shape=(34,)),
-        keras.layers.Dense(8, activation=tf.nn.sigmoid),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+        model2.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
+        # parece el mejor
+        model3 = keras.Sequential([
+            keras.layers.Flatten(input_shape=(1500, 300)),
+            keras.layers.Dense(8, activation=tf.nn.sigmoid),
+            keras.layers.Dense(8, activation=tf.nn.sigmoid),
+            keras.layers.Dense(8, activation=tf.nn.relu),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-    model0.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+        model3.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
-    model1 = keras.Sequential([
-        keras.layers.Dense(26, input_shape=(34,)),
-        keras.layers.Dense(16, activation=tf.nn.sigmoid),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+        trainable_models.append((model0, 15))
+        trainable_models.append((model0, 40))
+        trainable_models.append((model1, 30))
+        trainable_models.append((model1, 60))
+        trainable_models.append((model2, 60))
+        trainable_models.append((model2, 80))
+        trainable_models.append((model3, 80))
+        trainable_models.append((model3, 160))
 
-    model1.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+        return trainable_models
 
-    model2 = keras.Sequential([
-        keras.layers.Flatten(input_shape=(1500, 300)),
-        keras.layers.Dense(16, activation=tf.nn.sigmoid),
-        keras.layers.Dense(16, activation=tf.nn.relu),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+    @staticmethod
+    def lbp_models():
+        trainable_models = []
 
-    model2.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
-    # parece el mejor
-    model3 = keras.Sequential([
-        keras.layers.Flatten(input_shape=(1500, 300)),
-        keras.layers.Dense(8, activation=tf.nn.sigmoid),
-        keras.layers.Dense(8, activation=tf.nn.sigmoid),
-        keras.layers.Dense(8, activation=tf.nn.relu),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
+        model0 = keras.Sequential([
+            keras.layers.Dense(26, input_shape=(34,)),
+            keras.layers.Dense(8, activation=tf.nn.sigmoid),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-    model3.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+        model0.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
-    trainable_models.append((model0, 80))
-    trainable_models.append((model0, 160))
-    trainable_models.append((model0, 380))
-    trainable_models.append((model1, 160))
-    trainable_models.append((model1, 380))
-    trainable_models.append((model1, 760))
+        model1 = keras.Sequential([
+            keras.layers.Dense(26, input_shape=(34,)),
+            keras.layers.Dense(16, activation=tf.nn.sigmoid),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-    return trainable_models
+        model1.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
+        model2 = keras.Sequential([
+            keras.layers.Flatten(input_shape=(1500, 300)),
+            keras.layers.Dense(16, activation=tf.nn.sigmoid),
+            keras.layers.Dense(16, activation=tf.nn.relu),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-def train_model(model, train_set, train_labels, test_set, test_labels, epochs, weights):
-    shuffle_weights(model, weights=weights)
-    model.summary()
+        model2.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
+        # parece el mejor
+        model3 = keras.Sequential([
+            keras.layers.Flatten(input_shape=(1500, 300)),
+            keras.layers.Dense(8, activation=tf.nn.sigmoid),
+            keras.layers.Dense(8, activation=tf.nn.sigmoid),
+            keras.layers.Dense(8, activation=tf.nn.relu),
+            keras.layers.Dense(5, activation=tf.nn.softmax)
+        ])
 
-    history = model.fit(train_set, train_labels, epochs=epochs)
-    test_loss, test_acc = model.evaluate(test_set, test_labels)
+        model3.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
-    model.save("testModel.h5")
+        trainable_models.append((model0, 80))
+        trainable_models.append((model0, 160))
+        trainable_models.append((model0, 380))
+        trainable_models.append((model1, 160))
+        trainable_models.append((model1, 380))
+        trainable_models.append((model1, 760))
 
-    print('Test accuracy:', test_acc)
+        return trainable_models
 
-    return test_acc, history
+    def train_model(self, model, train_set, train_labels, test_set, test_labels, epochs, weights):
+        self.shuffle_weights(model, weights=weights)
+        model.summary()
 
+        history = model.fit(train_set, train_labels, epochs=epochs)
+        test_loss, test_acc = model.evaluate(test_set, test_labels)
 
-def shuffle_weights(model, weights=None):
-    if weights is None:
-        weights = model.get_weights()
-    weights = [np.random.permutation(w.flat).reshape(w.shape) for w in weights]
-    # Faster, but less random: only permutes along the first dimension
-    # weights = [np.random.permutation(w) for w in weights]
-    model.set_weights(weights)
+        print('Test accuracy:', test_acc)
+        self.save_if_the_best(test_acc, model)
+
+        return test_acc, history
+
+    def save_if_the_best(self, accuracy, model):
+        if accuracy > self.best_score:
+            accuracy = int(accuracy*100)
+
+            model.save("bestModel" + str(accuracy) + ".h5")
+            self.best_score = accuracy
+
+    @staticmethod
+    def shuffle_weights(model, weights=None):
+        if weights is None:
+            weights = model.get_weights()
+        weights = [np.random.permutation(w.flat).reshape(w.shape) for w in weights]
+        # Faster, but less random: only permutes along the first dimension
+        # weights = [np.random.permutation(w) for w in weights]
+        model.set_weights(weights)
